ee45917630128229b733782fde1974efe71f755a
NiklasRestory <niklas.cullberg@restory.ai>
2022-12-20 09:37:33 +0100

    First Commit

---
+++ src/main/java/com/company/December20CICDDockerLesson/December20CicdDockerLessonApplication.java
@@ -0,0 +1,14 @@
+package com.company.December20CICDDockerLesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class December20CicdDockerLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(December20CicdDockerLessonApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/company/December20CICDDockerLesson/December20CicdDockerLessonApplicationTests.java
@@ -0,0 +1,22 @@
+package com.company.December20CICDDockerLesson;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class December20CicdDockerLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	public void helloTest() {
+		HelloController helloController = new HelloController();
+		String expected = "Hello!";
+		String actual = helloController.getHello();
+		assertEquals(expected, actual);
+	}
+}
