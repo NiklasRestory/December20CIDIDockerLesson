d6346e75427456cc20c16371e5bb605dfcca353b
NiklasRestory <niklas.cullberg@restory.ai>
2022-12-20 10:20:35 +0100

    Added failing test.

--- src/test/java/com/company/December20CICDDockerLesson/December20CicdDockerLessonApplicationTests.java
+++ src/test/java/com/company/December20CICDDockerLesson/December20CicdDockerLessonApplicationTests.java
@@ -1,6 +1,7 @@
 package com.company.December20CICDDockerLesson;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,5 +19,10 @@ class December20CicdDockerLessonApplicationTests {
 		String expected = "Hello!";
 		String actual = helloController.getHello();
 		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void failingTest() {
+		fail();
 	}
 }
